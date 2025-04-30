d886fda7a36bf806774d8f7280531060096c7a95
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-01 01:47:59 +0600

    destructuring done

---
+++ modules/module_01/src/1.8.ts
@@ -0,0 +1,37 @@
+{
+  // destructuring
+  const user = {
+    id: 45192,
+    name: {
+      firstName: "Rownak",
+      middleName: "Abdullah",
+      lastName: "Omi",
+    },
+    contactNo: "01500000000",
+    address: "Dhaka",
+  };
+
+  const {
+    contactNo : phoneNo,
+    name: { middleName : midName },
+  } = user;
+
+
+//   array
+const myFriends = ["rahim", "karim", "hasan", "rahman", "mahmud", "rasel"];
+
+const [,,bestFriend, ...restOtherFriends] = myFriends;
+
+console.log(bestFriend, restOtherFriends);
+
+
+
+
+
+
+
+
+
+
+
+}
