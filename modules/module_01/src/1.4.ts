900db3e5a1e16e88ab8521a7c2b11317995b2f2e
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-04-30 22:01:16 +0600

    defining data types

---
+++ modules/module_01/src/1.4.ts
@@ -0,0 +1,19 @@
+let firstName: string = "Rownak";
+
+let id: number = 1234;
+
+let isAdmin: boolean = true;
+
+let x: undefined = undefined;
+
+let y: null = null;
+
+let friends: string[] = ["aminur", "tonmoy", "sajan"];
+
+let eligibleRolls: number[] = [321, 453, 145, 673];
+
+// tuple ---> Special Array
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [30, "Mr. XZ", true];
