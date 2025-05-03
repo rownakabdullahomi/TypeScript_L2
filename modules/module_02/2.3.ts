20b6430fad803b2b01c6f0f70a9c6edaca73fffd
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-03 22:26:14 +0600

    generic type in ts part 1 done

--- modules/module_02/2.3.ts
+++ modules/module_02/2.3.ts
@@ -1,40 +1,36 @@
 {
-// generic type
+  // generic type
 
-type GenericArray<T> = Array<T>
+  type GenericArray<T> = Array<T>;
 
-// const rollNumbers: number[] = [2, 5, 8];
-const rollNumbers: GenericArray<number> = [2, 5, 8];
+  // const rollNumbers: number[] = [2, 5, 8];
+  const rollNumbers: GenericArray<number> = [2, 5, 8];
 
-// const mentors: string[] = ['mr. x', 'mr. y', 'mr. z'];
-const mentors: GenericArray<string> = ['mr. x', 'mr. y', 'mr. z'];
+  // const mentors: string[] = ['mr. x', 'mr. y', 'mr. z'];
+  const mentors: GenericArray<string> = ["mr. x", "mr. y", "mr. z"];
 
-// const boolArray: boolean[] = [true, false, true];
-const boolArray: GenericArray<boolean> = [true, false, true];
+  // const boolArray: boolean[] = [true, false, true];
+  const boolArray: GenericArray<boolean> = [true, false, true];
 
-
-const user: GenericArray<{name: string, age: number}>= [
+  const user: GenericArray<{ name: string; age: number }> = [
     {
-        name: "Rownak",
-        age: 30,
+      name: "Rownak",
+      age: 30,
     },
     {
-        name: "Jhankar",
-        age: 45,
-    }
-]
+      name: "Jhankar",
+      age: 45,
+    },
+  ];
 
+  // generic tuple
 
+  type GenericTuple<X, Y> = [X, Y];
 
+  const manush: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
 
-
-
-
-
-
-
-
-
-
-
+  const userWithID: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "Rownak", email: "a@gmail.com" },
+  ];
 }
