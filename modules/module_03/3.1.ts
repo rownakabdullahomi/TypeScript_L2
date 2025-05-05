34c8b4026efc3f73624769b707c07caaf67935b8
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-05 13:52:13 +0600

    oop in ts started

---
+++ modules/module_03/3.1.ts
@@ -0,0 +1,39 @@
+{
+// oop- class
+
+class Animal{
+    name: string;
+    species: string;
+    sound: string;
+
+    constructor(name: string, species: string, sound: string){
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+}
+
+const dog = new Animal('german shepard', 'dog', 'ghew ghew');
+const cat = new Animal('parsian', 'cat', 'miaw miaw');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
