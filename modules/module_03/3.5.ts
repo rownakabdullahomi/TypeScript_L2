36a9777070157e632de5181bf27351383b27ce1a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-06 12:03:19 +0600

    access modifiers done

---
+++ modules/module_03/3.5.ts
@@ -0,0 +1,69 @@
+{
+// access modifiers
+class BankAccount{
+    readonly id: number;
+    name: string;
+    protected _balance: number;
+
+
+    constructor(id: number, name: string, balance: number){
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+    }
+
+    addDeposit(amount: number){
+        this._balance = this._balance + amount;
+    }
+
+    getBalance (){
+        return this._balance;
+    }
+}
+
+class studentAccount extends BankAccount{
+    test(){
+        this._balance
+    }
+}
+
+const poorAccount = new BankAccount(111, "Mr. Poor", 20);
+poorAccount.addDeposit(200);
+
+const myBalance = poorAccount.getBalance();
+console.log(myBalance);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
