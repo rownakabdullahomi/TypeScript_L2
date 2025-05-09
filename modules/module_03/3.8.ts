cda21acdbf187f718a3b50a9fedaf67f229fdf7c
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-09 19:57:53 +0600

    polymorphism started

---
+++ modules/module_03/3.8.ts
@@ -0,0 +1,56 @@
+{
+// polymorphism
+
+class Person {
+    getSleep(){
+        console.log(`I am sleeping 8 hours per day`);
+    }
+}
+
+class Student extends Person {
+    getSleep(){
+        console.log(`I am sleeping 7 hours per day`);
+    }
+}
+class Developer extends Person {
+    getSleep(){
+        console.log(`I am sleeping 6 hours per day`);
+    }
+}
+
+const getSleepingHours = (param : Person) => {
+    param.getSleep();
+}
+
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new Developer();
+
+getSleepingHours(person1);
+getSleepingHours(person2);
+getSleepingHours(person3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
