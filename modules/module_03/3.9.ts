30435dbcf4e9adc61a143650c128fc80fdc40258
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-10 13:46:24 +0600

    abstraction done

--- modules/module_03/3.9.ts
+++ modules/module_03/3.9.ts
@@ -21,13 +21,13 @@
       console.log(`I am starting the engine`);
     }
     stopEngine(): void {
-        console.log(`I am stopping the engine`);
+      console.log(`I am stopping the engine`);
     }
     move(): void {
-        console.log(`I am moving`);
+      console.log(`I am moving`);
     }
-    test(){
-        console.log(`I am testing`);
+    test() {
+      console.log(`I am testing`);
     }
   }
 
@@ -36,6 +36,52 @@
   toyotaCar.stopEngine();
   toyotaCar.move();
   toyotaCar.test();
+
+  // abstract class (idea)
+  abstract class Vehicle2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+    test() {
+      console.log(`I am testing`);
+    }
+  }
+
+  class TataCar extends Vehicle2{
+    startEngine(): void {
+        console.log(`Tata car is starting the engine`);
+    }
+    stopEngine(): void {
+      console.log(`Tata car is stopping the engine`);
+    }
+    move(): void {
+      console.log(`Tata car is moving`);
+    }
+  }
+
+//   const hondaCar = new Vehicle2();
+
+//   hondaCar.startEngine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
