d98ab98c8cbeb57df138da26d3671fd00fd3debe
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-11 20:40:20 +0600

    oop conceptual started

---
+++ modules/OOP_Conceptual/main.ts
@@ -0,0 +1,21 @@
+class Product {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+
+  constructor(id: number, name: string, price: number, quantity: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  getDetails(): string {
+    return `Product: ${this.name} - $${this.price} stock: ${this.quantity}`;
+  }
+
+  getStockValue(): number {
+    return this.price * this.quantity;
+  }
+}
