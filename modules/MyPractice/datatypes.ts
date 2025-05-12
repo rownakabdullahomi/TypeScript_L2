53409092efa44fce2bd3f6f154ba996f96a76450
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-12 21:51:39 +0600

    arrays and tuples practice

--- modules/MyPractice/datatypes.ts
+++ modules/MyPractice/datatypes.ts
@@ -1,4 +1,22 @@
 // Primitive Data Types
-
 // String
 let studentName: string = "Alice";
+studentName = 'Bob';
+
+// Number
+const age: number = 28;
+
+// Boolean
+let isPassed: boolean = true;
+
+// ------------------------------
+// Arrays and Tuples
+// Arrays
+let scores: number[] = [85, 90, 77, 75];
+let names: string[] = ['Rahim', 'Karim', 'Hasan', 'Abul'];
+let scores2: Array<number> = [65, 58, 87, 83]
+console.log(scores, names, scores2);
+
+// Tuples (fixed length array)
+let students: [string, number, boolean] = ['Alice', 21, true];
+console.log(`Name: ${students[0]}, Age: ${students[1]}, isPassed: ${students[2]}`);
